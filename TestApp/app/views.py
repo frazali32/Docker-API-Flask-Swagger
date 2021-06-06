fab29d59d1a0d2422a33f2979ea66fdf5113cb48
frazali32 <61795683+frazali32@users.noreply.github.com>
2021-06-06 09:23:36 -0400

    Add files via upload

---
+++ TestApp/app/views.py
@@ -0,0 +1,33 @@
+from app import app
+from flask import render_template
+from flask import request
+from flask_swagger_ui import get_swaggerui_blueprint
+
+@app.route('/')
+def home():
+   return "hello no world!"
+
+@app.route('/template',methods=['POST','GET'])
+def template():
+    output = request.args.values()
+    return render_template('home.html',prediction_text="Hello {}?".format(list(output)[:]))
+    
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+@app.route('/predict')
+def predict():
+    return render_template('index.html')
+
+
